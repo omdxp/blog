88326450af1785c3c39f1346ace72cf4bb9070db
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-09 19:14:21 +0100

    show post from url slug

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,9 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati repudiandae dolor perspiciatis explicabo eveniet minus. Incidunt velit, labore vitae iure asperiores tenetur architecto tempore expedita in assumenda dolor ad harum.</p>
+        <?= $post; ?>
     </article>
     <a href="/">Go back</a>
 </body>
