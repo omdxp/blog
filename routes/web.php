44cea4c4615bb9fbffe99c818456365ef8d5f9df
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-09 19:25:48 +0100

    caching views for every 20 minutes

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,15 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
         // abort(404);
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("post.{$slug}", now()->addMinutes(20), function () use ($path) {
+        return file_get_contents($path);
+    });
 
     return view("post", [
         "post" => $post
